fd20b029d8c1274fed6d5e1276a7a5000566ce2a
mike-ward <mike@wardfam.org>
2018-11-16 15:20:05 -0500

    update build script

--- src/build.cmd
+++ src/build.cmd
@@ -1,6 +1,5 @@
 nuget restore
-msbuild app.sln /t:Clean;Rebuild "/p:configuration=Release" /verbosity:minimal
 pushd App\js
 call npm i
-call npm run build
 popd
+msbuild app.sln /t:Clean;Rebuild "/p:configuration=Release" /verbosity:minimal
