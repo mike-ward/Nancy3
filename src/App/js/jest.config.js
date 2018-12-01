1fdd7de33be44e345fb6a9c7216998f05b4fc852
mike-ward <mike@mike-ward.net>
2018-12-01 16:45:48 -0500

    fix resharper by allowing synthetic imports

--- src/App/js/jest.config.js
+++ src/App/js/jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        "target": 'es5',
+        "module": 'commonjs',
+        "esModuleInterop": true,
+        "sourceMap": true,
+        "outDir": '../tsout'
+      }
+    }
+  }
 };
