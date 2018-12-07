dfda37224d8facee54d668667fc44d71f226c7d2
mike-ward <mike@mike-ward.net>
2018-12-07 10:01:36 -0500

    some more tests to practice with xUnit

--- src/App/Infrastructure/Extensions/ObjectExtensions.cs
+++ src/App/Infrastructure/Extensions/ObjectExtensions.cs
@@ -8,9 +8,19 @@ namespace App.Infrastructure.Extensions
         public static string ToStringInvariant(this object item)
         {
             // Insure numbers like 7E-06 are expressed as 0.000007
-            return item is double d
-                ? d.ToString("0.###################", CultureInfo.InvariantCulture)
-                : Convert.ToString(item, CultureInfo.InvariantCulture);
+            if (item is double dbl)
+            {
+                return dbl.ToString("0.###################", CultureInfo.InvariantCulture);
+            }
+
+            // Dates are always expressed in universal time format
+            if (item is DateTime date)
+            {
+                return date.ToUniversalTime().ToString("u", CultureInfo.InvariantCulture);
+            }
+            
+            // Other stuff is just invariant culture
+            return Convert.ToString(item, CultureInfo.InvariantCulture);
         }
     }
 }
---
+++ src/Test/Infrastructure/Extensions/ObjectExtensionTests.cs
@@ -0,0 +1,31 @@
+﻿using App.Infrastructure.Extensions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Test.Infrastructure.Extensions
+{
+    public class ObjectExtensionTests
+    {
+        [Fact]
+        public void DoubleToStringTests()
+        {
+            7e-10.ToStringInvariant().Should().Be("0.0000000007");
+            7e5.ToStringInvariant().Should().Be("700000");
+        }
+
+        [Fact]
+        public void DateToStringTest()
+        {
+            var date = new DateTime(2018, 12, 3, 11, 59, 0, DateTimeKind.Utc);
+            date.ToStringInvariant().Should().Be("2018-12-03 11:59:00Z");
+        }
+
+        [Fact]
+        public void StringToStringTests()
+        {
+            "Trump".ToStringInvariant().Should().Be("Trump");
+            "Mir geht es gut, danke!".Should().Be("Mir geht es gut, danke!");
+        }
+    }
+}
