e105de839fef9343e925c19d6745cd79d48df2a9
mike-ward <mike@mike-ward.net>
2018-12-08 14:27:44 -0500

    move sorting logic to seperate module

---
+++ src/App/js/components/grid/gridSort.ts
@@ -0,0 +1,44 @@
+﻿import { IGridModel } from "./IGridModel";
+import { ISortByColumn, ISortByMap } from "./gridViewModel";
+import { compareService } from '../../services/compare-service';
+
+export function sortedBy(sortByState: ISortByColumn[]) {
+  const map = sortByState.reduce<ISortByMap>(
+    (a, c, i) => {
+      a[c.id] = { direction: c.direction, ordinal: i };
+      return a;
+    }, {});
+  return map;
+}
+
+export function updateSortState(columnId: string, sortByState: ISortByColumn[]) {
+  const sortBy = sortByState.reduce((a, c) => c.id === columnId ? c : a, null);
+  if (!sortBy) sortByState = [{ id: columnId, direction: 1 }];
+  else if (sortBy.direction > 0) sortBy.direction = -1;
+  else if (sortBy.direction < 0) sortByState = [];
+  return sortByState;
+}
+
+export function sortByColumns(model: IGridModel, sortByState: ISortByColumn[]) {
+  for (let sortByColumn of sortByState) {
+    // future: add multiple column sort
+    const column = model.columns.reduce((a, c) => c.id === sortByColumn.id ? c : a, null);
+    if (!column) return model.data;
+
+    const comparer = column.comparer
+      ? column.comparer
+      : compareService.compareAny;
+
+    const columnId = column.id;
+    const direction = sortByColumn.direction;
+    const data = model.data.slice();
+
+    data.sort((l: any, r: any) => {
+      const result = comparer(l[columnId], r[columnId]);
+      return direction >= 0 ? result : -result;
+    });
+
+    return data;
+  }
+  return model.data;
+}
--- src/App/js/components/grid/gridViewModel.ts
+++ src/App/js/components/grid/gridViewModel.ts
@@ -1,13 +1,13 @@
 ﻿import stream from 'mithril/stream';
-import { IGridModel, IGridColumn } from './IGridModel';
-import { compareService } from '../../services/compare-service';
+import { IGridModel } from './IGridModel';
+import { sortByColumns, updateSortState, sortedBy } from './gridSort';
 
-interface ISortByColumn {
+export interface ISortByColumn {
   id: string;
   direction: number;
 }
 
-interface ISortByMap {
+export interface ISortByMap {
   [id: string]: { direction: number, ordinal: number }
 }
 
@@ -18,9 +18,10 @@ export interface IGridViewModel extends IGridModel {
 
 export function gridViewModelStream(gridModelStream: stream.Stream<IGridModel>) {
   let sortByState = [] as ISortByColumn[];
+
   const gridViewModelStream = gridModelStream.map<IGridViewModel>(gm => gm &&
     ({
-      columns: visibleColumns(gm.columns),
+      columns: gm.columns.filter(c => !c.hide),
       data: sortByColumns(gm, sortByState),
       key: gm.key,
       sortedBy: sortedBy(sortByState),
@@ -29,51 +30,6 @@ export function gridViewModelStream(gridModelStream: stream.Stream<IGridModel>) 
         gridModelStream(gm);
       }
     }));
+
   return gridViewModelStream;
-}
-
-function visibleColumns(columns: IGridColumn[]) {
-  const filtered = columns.filter(c => !c.hide);
-  return filtered;
-}
-
-function sortedBy(sortByState: ISortByColumn[]) {
-  const map = sortByState.reduce<ISortByMap>(
-    (a, c, i) => {
-      a[c.id] = { direction: c.direction, ordinal: i };
-      return a;
-    }, {});
-  return map;
-}
-
-function updateSortState(columnId: string, sortByState: ISortByColumn[]) {
-  const sortBy = sortByState.reduce((a, c) => c.id === columnId ? c : a, null);
-  if (!sortBy) sortByState = [{ id: columnId, direction: 1 }];
-  else if (sortBy.direction > 0) sortBy.direction = -1;
-  else if (sortBy.direction < 0) sortByState = [];
-  return sortByState;
-}
-
-function sortByColumns(model: IGridModel, sortByState: ISortByColumn[]) {
-  for (let sortByColumn of sortByState) {
-    // future: add multiple column sort
-    const column = model.columns.reduce((a, c) => c.id === sortByColumn.id ? c : a, null);
-    if (!column) return model.data;
-
-    const comparer = column.comparer
-      ? column.comparer
-      : compareService.compareAny;
-
-    const columnId = column.id;
-    const direction = sortByColumn.direction;
-    const data = model.data.slice();
-
-    data.sort((l: any, r: any) => {
-      const result = comparer(l[columnId], r[columnId]);
-      return direction >= 0 ? result : -result;
-    });
-
-    return data;
-  }
-  return model.data;
 }
