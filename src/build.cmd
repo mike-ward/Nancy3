df2427086898332895ef57ba6e255dc143919ad3
mike-ward <mike@mike-ward.net>
2019-01-17 09:39:45 -0500

    add js/css assets to deploy

--- src/build.cmd
+++ src/build.cmd
@@ -3,3 +3,4 @@ pushd App\js
 call npm i
 popd
 msbuild App.sln /t:Clean;Rebuild /p:configuration=Release /p:DeployOnBuild=true /verbosity:minimal
+copy /Y App\wwwroot\content\*.*s App\bin\Release\netcoreapp2.1\Publish\wwwroot
